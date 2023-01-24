7a672df8c13b70a40e38a7860e660779c6ef6de8
Kirill_divine <kirill.sokolov96@gmail.com>
2023-01-24 22:18:11 +0200

    Moving Zeros To The End

---
+++ Training/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Training
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        //MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
+        public static int[] MoveZeroes(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    for (int x = i + 1; x < arr.Length; x++)
+                    {
+                        if (arr[x] != 0)
+                        {
+                            arr[i] = arr[x];
+                            arr[x] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
+}
