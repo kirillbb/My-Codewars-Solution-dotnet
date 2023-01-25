ef6a973bdd4ed3013b7656177be4263f397054dc
Kirill_divine <kirill.sokolov96@gmail.com>
2023-01-25 14:42:52 +0200

    calculate the average of the numbers in a given list

--- Training/Program.cs
+++ Training/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
+
+        }
+
+        //FindAverage(new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 });
+        public static double FindAverage(double[] array)
+        {
+            double result = 0;
+            if (array.Length != 0)
+            {
+                foreach (var item in array)
+                    result+= item;
+
+                return result / array.Length;
+            }
             
+            return result;
         }
 
         //MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
