96c5b0f4051205049ffdcd968d2a5c39ed874a66
Kirill_divine <kirill.sokolov96@gmail.com>
2023-01-26 12:39:14 +0200

    delete comments

--- Training/Program.cs
+++ Training/Program.cs
@@ -7,14 +7,11 @@
             
         }
 
-        //getMiddle("test") should return "es"
-        //getMiddle("testing") should return "t"
         public static string GetMiddle(string s)
         {
             return s.Length % 2 != 0 ? s[s.Length / 2].ToString() : s[s.Length / 2 - 1].ToString() + s[s.Length / 2].ToString();
         }
 
-        //FindAverage(new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 });
         public static double FindAverage(double[] array)
         {
             double result = 0;
@@ -29,7 +26,6 @@
             return result;
         }
 
-        //MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
         public static int[] MoveZeroes(int[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
