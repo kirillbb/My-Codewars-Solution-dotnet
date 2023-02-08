63e71831aedd264ffe58cfd86d855e00b1f5f545
kirillbb <kirill.sokolov96@gmail.com>
2023-02-08 23:29:35 +0200

    moved the functions to separate files in the Classes folder

---
+++ Training/Classes/Kata.cs
@@ -0,0 +1,48 @@
+﻿namespace Training.Classes
+{
+    public class Kata
+    {
+        public static int DescendingOrder(int num)
+        {
+            return int.Parse(string.Concat(num.ToString()
+                .OrderByDescending(x => x)));
+        }
+
+        public static string GetMiddle(string s)
+        {
+            return s.Length % 2 != 0
+                ? s[s.Length / 2].ToString()
+                : s[s.Length / 2 - 1].ToString() + s[s.Length / 2].ToString();
+        }
+        public static string ToCamelCase(string str)
+        {
+            return string.Concat(str.Split('-', '_').Select((s, i) => i > 0 ? char.ToUpper(s[0]) + s.Substring(1) : s));
+        }
+
+        public static int SumTwoSmallestNumbers(int[] numbers)
+        {
+            return numbers.OrderBy(i => i).Take(2).Sum();
+        }
+
+        public static int[] MoveZeroes(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    for (int x = i + 1; x < arr.Length; x++)
+                    {
+                        if (arr[x] != 0)
+                        {
+                            arr[i] = arr[x];
+                            arr[x] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ Training/Classes/ListFilterer.cs
@@ -0,0 +1,20 @@
+﻿namespace Training.Classes
+{
+    public class ListFilterer
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var item in listOfItems)
+            {
+                if (item is int)
+                {
+                    result.Add((int)item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
