12c5fb28ea868fadce4edbbd37fffa2a2a36dffd
kirillbb <kirill.sokolov96@gmail.com>
2023-02-09 20:18:01 +0200

    DnaStrand test

---
+++ Training.Tests/DnaStrandTests.cs
@@ -0,0 +1,16 @@
+﻿using Training.Classes;
+using Xunit;
+
+namespace Training.Tests
+{
+    public class DnaStrandTests
+    {
+        [Fact]
+        public void MakeComplementSimpleTests()
+        {
+            Assert.Equal("ATGC", DnaStrand.MakeComplement("TACG"));
+            Assert.Equal("TATAG", DnaStrand.MakeComplement("ATATC"));
+            Assert.Equal("CCGGAT", DnaStrand.MakeComplement("GGCCTA"));
+        }
+    }
+}
