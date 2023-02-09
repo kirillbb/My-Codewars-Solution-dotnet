949aa8fed1ba0fe5126193c536d141d661c5528e
kirillbb <kirill.sokolov96@gmail.com>
2023-02-09 20:44:26 +0200

    Persist Persistence basic test

---
+++ Training.Tests/PersistTests.cs
@@ -0,0 +1,18 @@
+﻿using Training.Classes;
+using Xunit;
+
+namespace Training.Tests
+{
+    public class PersistTests
+    {
+        [Fact]
+        public void PersistenceBasicTests()
+        {
+            Assert.Equal(3, Persist.Persistence(39));
+            Assert.Equal(0, Persist.Persistence(4));
+            Assert.Equal(2, Persist.Persistence(25));
+            Assert.Equal(4, Persist.Persistence(999));
+            Assert.Equal(3, Persist.Persistence(444));
+        }
+    }
+}
