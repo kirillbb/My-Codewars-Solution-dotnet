c6b4758eafb62b3c4320614950c578a0431c70bd
kirillbb <kirill.sokolov96@gmail.com>
2023-02-11 22:54:44 +0200

    cleaning svm

--- Training/Program.cs
+++ Training/Program.cs
@@ -6,12 +6,7 @@ namespace Training
     {
         public static void Main(string[] args)
         {
-            var tower = TowerBuilder.Build(6);
-
-            for (int i = 0; i < tower.Length; i++)
-            {
-                Console.WriteLine("[" + tower[i] + "]");
-            }
+            
         }
     }
 }
