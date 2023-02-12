fe2c807121762170cb111bb15fd0697e0d791465
kirillbb <kirill.sokolov96@gmail.com>
2023-02-12 22:40:41 +0200

    cleaning svm

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,12 +1,10 @@
-﻿using Training.Classes;
-
-namespace Training
+﻿namespace Training
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(Kata.ToWeirdCase("hello my name is kirill!"));
+
         }
     }
 }
