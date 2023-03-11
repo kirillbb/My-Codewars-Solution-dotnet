0fc7626c98169c38eb46fb15ef83cf03ca6e4ac3
Kirill_divine <kirill.sokolov96@gmail.com>
2023-03-11 16:39:10 +0200

    trying to understand delegates

---
+++ Training/Classes/Account.cs
@@ -0,0 +1,33 @@
+﻿namespace Training.Classes
+{
+    public delegate void AccountHandler(string message);
+
+    public class Account
+    {
+        int balance;
+        AccountHandler handler;
+
+        public Account(int sum) => this.balance = sum;
+
+        public void Add(int sum) => this.balance += sum;
+
+        public void Take(int sum)
+        {
+            if (balance >= sum)
+            {
+                balance -= sum;
+
+                handler?.Invoke($"Со счета списано {sum} у.е.");
+            }
+            else
+            {
+                handler?.Invoke($"Недостаточно средств. Баланс: {this.balance} у.е.");
+            }
+        }
+
+        public void RegisterHandler(AccountHandler handler)
+        {
+            this.handler = handler;
+        }
+    }
+}
